dc5dfee993a8c572d08383926bc12174deabddd1
Nikhil Sachdeva <stratonov16@gmail.com>
2026-06-15 03:05:27 +0530

    Disable img and link ESLint rules (external R2 images, valid use case)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,14 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off",
+    },
+  },
+];
 
 export default eslintConfig;
